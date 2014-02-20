3182c8496cf0e93ed39e0d305431c8a6b90a5099
Ramiro Araujo <rama.araujo@gmail.com>
2014-02-20 18:53:24 -0300

    finished basic functionality

---
+++ pbpaste-image/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  pbpaste-image
+//  cli app for extracting image from clipboard and sending it to stdout as binary data
+//
+//  Created by Ramiro Araujo on 2/20/14.
+//  Copyright (c) 2014 Ramiro Araujo. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+int main(int argc, const char *argv[]) {
+
+    @autoreleasepool {
+
+        NSPasteboard *pasteBoard = [NSPasteboard generalPasteboard];
+        NSImage *image = [[NSImage alloc] initWithPasteboard:pasteBoard];
+        NSData *data = [NSBitmapImageRep
+                representationOfImageRepsInArray:[image representations]
+                                       usingType:NSPNGFileType
+                                      properties:nil];
+
+        NSFileHandle *handler = [NSFileHandle fileHandleWithStandardOutput];
+        [handler writeData:data];
+    }
+
+    return 0;
+}
